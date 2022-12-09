ce736e32977a5f4b6956d5811af42a075fd20825
Petr Kucera <petr@khome.cz>
2022-12-09 20:29:44 +0100

    Creates files `utils` for supporting functions

---
+++ src/utils.h
@@ -0,0 +1,16 @@
+#ifndef __UTILS_H
+#define __UTILS_H
+
+/**
+ * @brief Initialize all pins on ESP32-S2 Dev Board
+ *
+ */
+void all_pins_init();
+
+/**
+ * @brief Activate all pins on ESP32-S2 Dev Board
+ *
+ */
+void all_pins_test();
+
+#endif
