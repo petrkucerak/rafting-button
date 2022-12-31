78568ee10a9a24a54bb2074d6975b368263f966e
Petr Kucera <petr@khome.cz>
2022-12-31 12:17:22 +0100

    Document a echo function

--- src/ESP-NOW/esp-now.h
+++ src/ESP-NOW/esp-now.h
@@ -20,4 +20,18 @@ void format_mac_address(const uint8_t *mac_address, char *buffer, int max_size);
 
 void test_esp_now(uint8_t count_of_neighbours);
 
+/**
+ * @brief The goal of this function is send back receive message.
+ * The detail description of this function:
+ *    1. check if `esp_now_handler` is an empty (!isEmpty),
+ *    2. init a new peer connection,
+ *    3. copy the target mac address into a peer structure,
+ *    4. check if peer connecion with this current mac address doesn't exist,
+ *    5. if peer connection doesn't exist connect,
+ *    6. then sent a message back,
+ *    7. set `esp_now_handler` as empty (isEmpty = TRUE).
+ * TODO: mutexes?
+ */
+void esp_now_echo();
+
 #endif
