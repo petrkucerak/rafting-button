f2083bb1c83dce0c413c3889103a5b01b7f1da14
Petr Kucera <petr@khome.cz>
2023-01-17 00:21:11 +0100

    Create graphs

--- measure/data2graph.py
+++ measure/data2graph.py
@@ -2,38 +2,41 @@ import matplotlib.pyplot as plt
 import os
 
 dir_list = os.listdir("./measure/scenarios")
-for file in dir_list:
-   print("Read file: ", file)
-   path = "./measure/scenarios/" + file
-   f = open(path)
+for folder in dir_list:
+    folder_path = "./measure/scenarios/" + folder
+    print(folder_path)
+    sub_dir_list = os.listdir(folder_path)
+    for file in sub_dir_list:
+        print("Read file: ", file)
+        path = folder_path + "/" + file
+        f = open(path)
 
-   label = file.replace("-data.txt", "") + " scenario"
+        label = file.replace("-data.txt", "") + " scenario"
 
-   x = f.readline()
-   y = f.readline()
+        x = f.readline()
+        y = f.readline()
 
-   f.close()
+        f.close()
 
-   x = x.replace("time=[", "")
-   x = x.replace(",]\n", "")
-   y = y.replace("value=[", "")
-   y = y.replace(",]", "")
+        x = x.replace("time=[", "")
+        x = x.replace(",]\n", "")
+        y = y.replace("value=[", "")
+        y = y.replace(",]", "")
 
-   x = list(map(int, x.split(",")))
-   y = list(map(int, y.split(",")))
+        x = list(map(int, x.split(",")))
+        y = list(map(int, y.split(",")))
 
-   bins = 10000
+        bins = 10000
 
-   # plt.hist(y, bins=bins, density=True, histtype='step', cumulative=-1, label = label)
-   plt.plot(x, y, label = label)
+        # plt.hist(y, bins=bins, density=True, histtype='step', cumulative=-1, label = label)
+        plt.plot(x, y, label=label)
 
+    plt.xlabel("Time [μs]")
+    plt.ylabel("Number of messages")
+    plt.legend()
+    plt.grid(True)
+    plt.yscale('log')
+    plt.xscale('linear')
+    plt.title("Round-trip time with ESP NOW in different scenarios")
 
-plt.xlabel("Time [μs]")
-plt.ylabel("Number of messages")
-plt.legend()
-plt.grid(True)
-# plt.yscale('log')
-plt.xscale('linear')
-plt.title("Round-trip time with ESP NOW in different scenarios")
-
-plt.show()
+    plt.show()
