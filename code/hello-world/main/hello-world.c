ccd612e1d06d8d298236142dba8ca1c24fd1cdc5
petrkucerak <petr@khome.cz>
2023-03-27 18:34:07 +0200

    Remove unused string

--- code/hello-world/main/hello-world.c
+++ code/hello-world/main/hello-world.c
@@ -14,7 +14,7 @@ void app_main(void)
    esp_chip_info_t chip_info;
    uint32_t flash_size;
    esp_chip_info(&chip_info);
-   printf("This is %s chip with %d CPU core(s), WiFi%s%s%s, ",
+   printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
           CONFIG_IDF_TARGET, chip_info.cores,
           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
