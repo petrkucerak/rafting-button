ad4551a8f1d711cc9f2a3ca932eeae29eee773a6
petrkucerak <petr@khome.cz>
2023-04-03 18:06:29 +0200

    Create separe component for espnow functions

---
+++ code/measure-esp-now/components/espnow_utils/espnow_utils.h
@@ -0,0 +1,41 @@
+#ifndef ESPNOW_UTILS_H
+#define ESPNOW_UTILS_H
+
+#include <esp_types.h>
+#include <esp_now.h>
+
+/**
+ * @brief Initialize Wi-Fi for ESP-NOW
+ *
+ */
+void wifi_init(void);
+
+/**
+ * @brief Initialize ESP-NOW
+ *
+ * @return esp_err_t
+ */
+static esp_err_t custom_espnow_init(void);
+
+static esp_err_t custom_espnow_deinit(void);
+
+/**
+ * @brief     Callback function of sending ESPNOW data
+ * @param     mac_addr peer MAC address
+ * @param     status status of sending ESPNOW data (succeed or fail)
+ */
+static void espnow_send_cb(const uint8_t *mac_addr,
+                           esp_now_send_status_t status);
+
+/**
+ * @brief     Callback function of receiving ESPNOW data
+ * @param     esp_now_info received ESPNOW packet information
+ * @param     data received data
+ * @param     data_len length of received data
+ * @attention esp_now_info is a local variable，it can only be used in the
+ * callback.
+ */
+static void espnow_recv_cb(const esp_now_recv_info_t *esp_now_info,
+                           const uint8_t *data, int data_len);
+
+#endif // ESPNOW_UTILS_H
--- code/measure-esp-now/main/measure-esp-now.c
+++ code/measure-esp-now/main/measure-esp-now.c
@@ -1,71 +1,19 @@
 #include "measure-esp-now.h"
-#include "driver/gpio.h"
-#include "esp_chip_info.h"
-#include "esp_flash.h"
-#include "esp_log.h"
-#include "esp_netif.h"
-#include "esp_now.h"
-#include "esp_wifi.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "nvs_flash.h"
+#include "espnow_utils.h"
 #include "peripheral.h"
-#include "sdkconfig.h"
+#include <driver/gpio.h>
+#include <esp_chip_info.h>
+#include <esp_flash.h>
+#include <esp_log.h>
+#include <esp_netif.h>
+#include <esp_now.h>
+#include <esp_wifi.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include <inttypes.h>
+#include <nvs_flash.h>
+#include <sdkconfig.h>
 #include <stdio.h>
-
-static void wifi_init(void)
-{
-   ESP_ERROR_CHECK(esp_netif_init()); // init  TCP/IP stack layer
-
-   // create loop for system task like Wi-Fi
-   ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-   ESP_ERROR_CHECK(esp_wifi_init(&cfg)); // init Wi-Fi with default conf
-
-   ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-
-   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-   ESP_ERROR_CHECK(esp_wifi_start());
-   // ESP_ERROR_CHECK(esp_wifi_set_channel())
-}
-
-static void espnow_send_cb(const uint8_t *mac_addr,
-                           esp_now_send_status_t status)
-{
-   printf("ESP-NOW send callback has been called\n");
-}
-
-static void espnow_recv_cb(const esp_now_recv_info_t *esp_now_info,
-                           const uint8_t *data, int data_len)
-{
-   printf("ESP-NOW receive callback has been called\n");
-}
-
-static esp_err_t custom_espnow_init(void)
-{
-
-   // Initialzie ESP-NOW
-   ESP_ERROR_CHECK(esp_now_init());
-
-   // Register ESP-NOW callbacks
-   ESP_ERROR_CHECK(esp_now_register_send_cb(espnow_send_cb));
-   ESP_ERROR_CHECK(esp_now_register_recv_cb(espnow_recv_cb));
-
-   // Get the ESP-NOW version, not works
-   uint32_t version;
-   ESP_ERROR_CHECK(esp_now_get_version(&version));
-   printf("ESP-NOW version is: %ld\n", version);
-
-   return ESP_OK;
-}
-
-static esp_err_t custom_espnow_deinit(void)
-{
-   ESP_ERROR_CHECK(esp_now_deinit());
-   return ESP_OK;
-}
 
 void measure_broadcast_task(void)
 {
