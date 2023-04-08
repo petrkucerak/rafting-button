c84512c2291a70532a20f192956f591b1674f435
petrkucerak <petr@khome.cz>
2023-04-08 17:27:18 +0200

    Create files for the DS component

---
+++ code/measure-esp-now/components/distributed_protocols/distributed_protocols.h
@@ -0,0 +1,17 @@
+#ifndef DISTRIBUTED_PROTOCOLS_H
+#define DISTRIBUTED_PROTOCOLS_H
+
+/**
+ * The file describes the distributed system (DS), includes function, and define custom structures for the distributed system.
+ * 
+ * DISTRIBUTED SYSTEM DESCRIPTION
+ * The system provides 4 custom major features:
+ *    1. time synchronization
+ *    2. leader selection
+ *    3. the list of neighbors (in ESP-NOW terminology the peer liest) distribution
+ *    4. distribution of logs with DS events
+ * 
+ * 
+ */
+
+#endif // DISTRIBUTED_PROTOCOLS_H
