57bda53231fca3f045769b80c9f0f08526ede224
petrkucerak <petr@khome.cz>
2023-04-08 17:40:10 +0200

    Simple describtion

--- code/measure-esp-now/components/distributed_protocols/distributed_protocols.h
+++ code/measure-esp-now/components/distributed_protocols/distributed_protocols.h
@@ -1,17 +1,30 @@
+// The file describes the distributed system (DS), includes function, and define
+// custom structures for the distributed system.
+
+// DISTRIBUTED SYSTEM DESCRIPTION
+// The system provides 4 custom major features:
+//    1. time synchronization
+//    2. leader selection
+//    3. the list of neighbors (in ESP-NOW terminology the peer liest)
+//    distribution
+//    4. distribution of logs with DS events
+//
+
+// 1. TIME SYNCHRONZIATION
+//    Master                                 Slave
+//                      time                 
+//        ---------------------------------> 
+//                      time                     
+//        <---------------------------------
+//    RTT            time + RTT              set time
+//        --------------------------------->
+//                      ACK
+//        <---------------------------------
+
+
+// RTT ... round-trip time
+   
 #ifndef DISTRIBUTED_PROTOCOLS_H
 #define DISTRIBUTED_PROTOCOLS_H
-
-/**
- * The file describes the distributed system (DS), includes function, and define custom structures for the distributed system.
- * 
- * DISTRIBUTED SYSTEM DESCRIPTION
- * The system provides 4 custom major features:
- *    1. time synchronization
- *    2. leader selection
- *    3. the list of neighbors (in ESP-NOW terminology the peer liest) distribution
- *    4. distribution of logs with DS events
- * 
- * 
- */
 
 #endif // DISTRIBUTED_PROTOCOLS_H
