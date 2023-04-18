4cecfb9087a6ef5e705fde69baac1b4b0ca7ff0a
petrkucerak <petr@khome.cz>
2023-04-18 14:35:29 +0200

    Add graph description

--- code/simulation/visualize.py
+++ code/simulation/visualize.py
@@ -4,5 +4,12 @@ import matplotlib.pyplot as plt
 filename = "output.txt"
 data = np.loadtxt(filename, dtype=float, delimiter=',')
 
+fig, ax = plt.subplots(figsize=(12, 8))
+
+#adds a title and axes labels
+ax.set_title('Time synchronization in DS')
+ax.set_ylabel('Node time\n(ticks)')
+ax.set_xlabel('DS time\n(ns)')
+
 plt.plot(data)
 plt.show()
