1d6d574530098986aae2132dcf19640c74a7474c
petrkucerak <petr@khome.cz>
2023-04-27 12:00:55 +0200

    Create app template

--- code/esp-now-parameters/main/esp-now-parameters.c
+++ code/esp-now-parameters/main/esp-now-parameters.c
@@ -1,1 +1,29 @@
-void app_main(void) {}
+#include "esp-now-parameters.h"
+#include <driver/gpio.h>
+#include <esp_chip_info.h>
+#include <esp_flash.h>
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <inttypes.h>
+#include <nvs_flash.h>
+#include <sdkconfig.h>
+#include <stdio.h>
+#include <string.h>
+
+void app_main(void)
+{
+   // Init NVS
+   esp_err_t ret = nvs_flash_init();
+   if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+       ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+   }
+   ESP_ERROR_CHECK(ret);
+
+   // Ending rutine
+   printf("Restarting now!\n");
+   fflush(stdout);
+   esp_restart();
+}
