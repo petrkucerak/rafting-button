394a9784c73e945b312de4986dd4dcda1be6a31a
petrkucerak <petr@khome.cz>
2023-05-09 04:12:20 +0200

    Add D node

--- code/simulation/visualizeD.py
+++ code/simulation/visualizeD.py
@@ -13,12 +13,15 @@ ax.set_xlabel('Time\n(ms)')
 
 plt.plot(data[:, 0]/10, data[:, 1]/10, label="SLAVE [B]")
 plt.plot(data[:, 0]/10, data[:, 4]/10, label="SLAVE [C]")
+plt.plot(data[:, 0]/10, data[:, 7]/10, label="SLAVE [D]")
 
 plt.axhline(y=np.mean(data[:,1]/10), color='green', label="SLAVE [B] mean")
 plt.axhline(y=np.mean(data[:,4]/10), color='brown', label="SLAVE [C] mean")
+plt.axhline(y=np.mean(data[:,7]/10), color='brown', label="SLAVE [D] mean")
 
 print("B mean",np.mean(data[:,1]/10))
 print("C mean",np.mean(data[:,4]/10))
+print("C mean",np.mean(data[:,7]/10))
 
 plt.legend()
 plt.grid(True)
