e2555ff3ce2a1674d2e2d68cd94904bdf2a2f0f9
petrkucerak <petr@khome.cz>
2023-05-11 21:54:44 +0200

    Remove unsued scripts

--- code/simulation/get_mean.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-filename = "output.txt"
-data = np.loadtxt(filename, dtype=float, delimiter=',')
-
-print(np.mean(data[:,1]/10))
-print(np.mean(data[:,2]/10))
