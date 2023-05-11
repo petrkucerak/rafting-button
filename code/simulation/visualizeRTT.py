1c801aa66771161aab86c8d0bc18d581377a9782
Petr Kučera <petr@khome.cz>
2023-05-11 23:08:08 +0200

    32 improve DS simulation (#33)
    * Add timer deviation
    
    * Set strcited time_speed
    
    * Synch RTT value
    
    * Vizualize latency
    
    * Improve simulation withtout latency
    
    * sync
    
    * Merge branch 'main' of github.com:petrkucerak/rafting-button into 30-measure-parameters-into-the-ds-mode
    
    * Remove unused code
    
    * Create new clear code
    
    * Imrpove simulation adding the abs
    
    * Allow comments in the code
    
    * Add round function
    
    * Change float artihmetic to static uint
    
    * Aleluja!
    
    * sync
    
    * Add D node
    
    * Add F scenario
    
    * Add scenario
    
    * sync files
    
    * Add RTT screenshot
    
    * Add RTT and DEVIATION option
    
    * Optimised images with calibre/image-actions
    
    * sync files
    
    * sync files
    
    * Clean project and calcule RTT abs
    
    * Add TIME and O synchronization
    
    * Fix O~ calculation
    
    * Finish improving simulation
    
    * Start with cleaning simulation
    
    * Add doxygen into the header file
    
    * Clean main.c
    
    * Create define for constant
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ code/simulation/visualizeRTT.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+filename = "output.txt"
+data = np.loadtxt(filename, dtype=float, delimiter=',')
+
+fig, ax = plt.subplots(figsize=(12, 8))
+
+ax.set_title('RTT synchronization in DS')
+ax.set_ylabel('Difference between MASTER and SLAVE\n(ms)')
+ax.set_xlabel('Time\n(ms)')
+# ax.set_yscale('log')
+
+plt.plot(data[:, 0]/1000, data[:, 1]/1000, label="~RTT [B]")
+plt.plot(data[:, 0]/1000, data[:, 4]/1000, label="~RTT [C]")
+plt.plot(data[:, 0]/1000, data[:, 7]/1000, label="~RTT [D]")
+
+# plt.plot(data[:, 0]/10, data[:, 3]/10, label="RTT l [B]")
+# plt.plot(data[:, 0]/10, data[:, 9]/10, label="RTT l [C]")
+# plt.plot(data[:, 0]/10, data[:, 15]/10, label="RTT l [D]")
+
+
+
+# plt.axhline(y=np.mean(data[:,3]/10), color='green', label="latency [B] mean")
+# plt.axhline(y=np.mean(data[:,6]/10), color='brown', label="latency [C] mean")
+
+
+
+plt.legend()
+plt.grid(True)
+
+plt.show()
