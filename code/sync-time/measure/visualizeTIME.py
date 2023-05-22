6f7659d6ec7c694593dc622152731519a493873d
petrkucerak <petr@khome.cz>
2023-05-22 10:56:59 +0200

    Add measurement screanrios

--- code/sync-time/measure/visualizeTIME.py
+++ code/sync-time/measure/visualizeTIME.py
@@ -1,17 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-folder = "RTT_100_zoomed_reverse/"
+folder = "RTT_100_K_0/"
+# files = ["log_COM4.txt", "log_COM6.txt",
+#          "log_COM7.txt", "log_COM8.txt", "log_COM9.txt"]
 files = ["log_COM4.txt", "log_COM6.txt",
-         "log_COM7.txt", "log_COM8.txt", "log_COM9.txt"]
+         "log_COM7.txt"]
 filenames = [folder+files[0], folder+files[1],
-             folder+files[2], folder+files[3], folder+files[4]]
+             folder+files[2]]
+# filenames = [folder+files[0], folder+files[1],
+#              folder+files[2], folder+files[3], folder+files[4]]
 
 data0 = np.loadtxt(filenames[0], dtype=float, delimiter=',')
 data1 = np.loadtxt(filenames[1], dtype=float, delimiter=',')
 data2 = np.loadtxt(filenames[2], dtype=float, delimiter=',')
-data3 = np.loadtxt(filenames[3], dtype=float, delimiter=',')
-data4 = np.loadtxt(filenames[4], dtype=float, delimiter=',')
+# data3 = np.loadtxt(filenames[3], dtype=float, delimiter=',')
+# data4 = np.loadtxt(filenames[4], dtype=float, delimiter=',')
 
 fig, ax = plt.subplots(figsize=(12, 8))
 
@@ -21,10 +25,10 @@ ax.set_xlabel('Time\n(s)')
 # ax.set_yscale('log')
 
 # plt.plot(data0[:, 0]/1000, label="~RTT [COM4]")
-plt.plot((data0[:,2] - data1[:, 2])/1000, label="[COM6]")
-plt.plot((data0[:,2] - data2[:, 2])/1000, label="[COM7]")
-plt.plot((data0[:,2] - data3[:, 2])/1000, label="[COM8]")
-plt.plot((data0[:,2] - data4[:, 2])/1000, label="[CON9]")
+plt.plot((data0[:, 2] - data1[:, 2])/1000, label="[COM6]")
+plt.plot((data0[:, 2] - data2[:, 2])/1000, label="[COM7]")
+# plt.plot((data0[:,2] - data3[:, 2])/1000, label="[COM8]")
+# plt.plot((data0[:,2] - data4[:, 2])/1000, label="[CON9]")
 
 # plt.plot(data0[:, 2]/1000, label="~O [COM4]")
 # plt.plot(data1[:, 2]/1000, label="~O [COM6]")
