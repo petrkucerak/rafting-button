9bb0ec40f21eb146ed6f4a2294ad0ce4e5b23a8a
petrkucerak <petr@khome.cz>
2023-05-22 18:52:20 +0200

    Archive old measurement

--- code/sync-time/measure/visualizeTIME.py
+++ code/sync-time/measure/visualizeTIME.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-folder = "RTT_100_E_300_V2/"
+folder = "RTT_100V1/"
 # files = ["log_COM4.txt", "log_COM6.txt",
 #          "log_COM7.txt", "log_COM8.txt", "log_COM9.txt"]
 files = ["log_COM4.txt", "log_COM6.txt",
