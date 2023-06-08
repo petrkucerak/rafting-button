a04c2c9c6c9b3d33f0298a5a74e1c8fa458e9ed4
petrkucerak <petr@khome.cz>
2023-06-09 00:00:58 +0200

    Document defines

--- code/rafting-button/main/rafting-button.h
+++ code/rafting-button/main/rafting-button.h
@@ -13,35 +13,59 @@
 #ifndef RAFTING_BUTTON_H
 #define RAFTING_BUTTON_H
 
+/// @brief Size of the array used for calculation round trip time.
 #define BALANCER_SIZE 100
+/// @brief The count of possible used devices is 10. Therefore the count of
+/// neighbors is 9 devices.
 #define NEIGHBOURS_COUNT 9
+/// @brief Size of the array used for storing logs with DS events.
 #define EVENT_HISTORY 50
 
+/// @brief Priority for the task that starts synchronization round trip time.
 #define PRIORITY_RTT_START 2
+/// @brief Priority for the task that starts synchronization DS time.
 #define PRIORITY_TIME_START 2
+/// @brief Priority for the task that handles ESP-NOW events.
 #define PRIORITY_HANDLER 4
+/// @brief Priority for the task that handles DS events, specifically logs.
 #define PRIORITY_HANDLE_DS_EVENT 2
+/// @brief Priority for the task that sends a message with a vote request.
 #define PRIORITY_REQUEST_TASK 2
+/// @brief Priority for the task that handles ISR events.
 #define PRIORITY_HANDLE_ISR_EVENT 3
 
+/// @brief Define the size of the queue used to handle ESP-NOW events.
 #define ESPNOW_QUEUE_SIZE 10
-#define PRINT_QUEUE_SIZE 4
-#define LOG_QUEUE_SIZE 10 // min, device count
+/// @brief Define the queue size used to handle DS events, specifically logs.
+/// Max size should be 10. DS is limited to 10 devices.
+#define LOG_QUEUE_SIZE 10
+/// @brief Define the queue size used to send timestamps from ISR.
 #define ISR_QUEUE_SIZE 1
 
+/// @brief Maximum processor tick count deadline for ESP-NOW events.
 #define ESPNOW_MAXDELAY 10
+/// @brief Maximum processor tick count deadline for reparetly sending timestamp
+/// from ISR.
 #define CLEANING_DELAY 1
+/// @brief Maximum processor tick count deadline for DS events, specifically log
+/// distribution.
 #define DS_MAXDELAY 100
+/// @brief Define stack size for tasks.
 #define STACK_SIZE 4096
 
+/// @brief Deviation limit used in time synchronization.
 #define DEVIATION_LIMIT 200
+/// @brief Deviation maximum constant used in time synchronization.
 #define DEVIATION_MAX_CONSTANT 25
-
+/// @brief Config universal ESP-NOW message length.
 #define CONFIG_ESPNOW_SEND_LEN 250
-
+/// @brief Define the limit of undelivered messages that indicates inactive
+/// devices.
 #define COUNT_ERROR_MESSAGE_TO_INACTIVE 5
 
+/// @brief Defines the vote timeout. The current value is 1s.
 #define VOTE_TIMEOUT 1000000
+/// @brief Defines the inactive master timeout. The current value is 2s.
 #define MASTER_TIMEOUT 2000000
 
 #include <espnow.h>
